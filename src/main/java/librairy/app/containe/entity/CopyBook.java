abf5644c470d5029ab2387290f5ab30e3beb2992
harena <tsutoru24@gmail.com>
2026-06-02 20:41:19 +0300

    feat : add BookCopy entity

--- src/main/java/librairy/app/containe/entity/CopyBook.java
+++ src/main/java/librairy/app/containe/entity/CopyBook.java
@@ -1,4 +1,21 @@
 package librairy.app.containe.entity;
 
-public class CopyBook {
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookCopy {
+
+    private int id;
+    private Book book;
+    private CopyStatus status;
+
+    public BookCopy() {}
+
+    public BookCopy(int id, Book book, CopyStatus status) {
+        this.id = id;
+        this.book = book;
+        this.status = status;
+    }
 }
