d52f8f1599a539f5a86ac0ad322f41bad7a39ad2
harena <tsutoru24@gmail.com>
2026-06-03 09:15:23 +0300

    change: change the name of the variable in entity

--- src/main/java/librairy/app/containe/entity/Book.java
+++ src/main/java/librairy/app/containe/entity/Book.java
@@ -1,21 +1,26 @@
 package librairy.app.containe.entity;
 
+import jakarta.persistence.Entity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 
 import java.time.LocalDate;
 
+@Entity
 @Getter
 @Setter
 @Builder
 public class Book {
-    private int Id;
-    private String Title;
-    private String Description;
-    private Double Price;
-    private LocalDate PublishDate;
-    private String ISBN;
-    private int CategoryId;
+    @Id
+
+
+    private String title;
+    private String description;
+    private Double price;
+    private LocalDate publishDate;
+    private String isbn;
+    private int categoryId;
 
 }
