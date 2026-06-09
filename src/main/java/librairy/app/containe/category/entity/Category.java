2ee3d050ddba7a21c83d38601b99ea58c2320647
valeria <hei.valeria.2@gmail.com>
2026-06-09 19:46:23 +0300

    feat: Add CategoryServiceTest

--- src/main/java/librairy/app/containe/category/entity/Category.java
+++ src/main/java/librairy/app/containe/category/entity/Category.java
@@ -1,11 +1,15 @@
 package librairy.app.containe.category.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "categories")
 public class Category {
