38bec0f7f6f5f2afb1c707376cb6afc582e298bb
harena <tsutoru24@gmail.com>
2026-06-10 20:27:12 +0300

    feat: add AuthorRepository

--- src/main/java/librairy/app/containe/Author/repository/AuthorRepository.java
+++ src/main/java/librairy/app/containe/Author/repository/AuthorRepository.java
@@ -1,4 +1,14 @@
 package librairy.app.containe.Author.repository;
 
-public class AuthorRepository {
+import librairy.app.containe.Author.entity.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface AuthorRepository
+        extends JpaRepository<Author, String> {
+
+    List<Author> findByLastName(String lastName);
+    List<Author> findByNationality(String nationality);
 }
