a73ed750933af609687647edb61a5cf4d2459ab8
harena <tsutoru24@gmail.com>
2026-06-10 20:27:51 +0300

    feat: add AuthorService

--- src/main/java/librairy/app/containe/Author/service/AuthorService.java
+++ src/main/java/librairy/app/containe/Author/service/AuthorService.java
@@ -1,4 +1,46 @@
 package librairy.app.containe.Author.service;
 
+import librairy.app.containe.Author.entity.Author;
+import librairy.app.containe.Author.repository.AuthorRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class AuthorService {
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    public Author create(Author author) {
+        return authorRepository.save(author);
+    }
+
+    public List<Author> getAll() {
+        return authorRepository.findAll();
+    }
+
+    public Author getById(String id) {
+        return authorRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Author not found"));
+    }
+
+    public List<Author> getByLastName(String lastName) {
+        return authorRepository.findByLastName(lastName);
+    }
+
+    public Author update(String id, Author newAuthor) {
+        Author author = getById(id);
+        author.setFirstName(newAuthor.getFirstName());
+        author.setLastName(newAuthor.getLastName());
+        author.setBiography(newAuthor.getBiography());
+        author.setNationality(newAuthor.getNationality());
+        return authorRepository.save(author);
+    }
+
+    public void delete(String id) {
+        authorRepository.deleteById(id);
+    }
 }
