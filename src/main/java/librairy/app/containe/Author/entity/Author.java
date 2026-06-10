64a49be58fdfe0fc70d56db838b1b38f559d1517
harena <tsutoru24@gmail.com>
2026-06-10 20:31:44 +0300

    style : change the format

--- src/main/java/librairy/app/containe/Author/entity/Author.java
+++ src/main/java/librairy/app/containe/Author/entity/Author.java
@@ -10,23 +10,23 @@ import lombok.Setter;
 @Setter
 public class Author {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private String id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private String id;
 
-    private String firstName;
-    private String lastName;
-    private String biography;
-    private String nationality;
+  private String firstName;
+  private String lastName;
+  private String biography;
+  private String nationality;
 
-    public Author() {}
+  public Author() {}
 
-    public Author(String id, String firstName, String lastName,
-                  String biography, String nationality) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.biography = biography;
-        this.nationality = nationality;
-    }
+  public Author(
+      String id, String firstName, String lastName, String biography, String nationality) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.biography = biography;
+    this.nationality = nationality;
+  }
 }
